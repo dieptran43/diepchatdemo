65f012622e3cbeb41f4540b05d8101997072a269
unknown <levandiep44@gmail.com>
2017-12-11 18:31:48 +0700

    update chat room

--- public/scripts/client2.js
+++ public/scripts/client2.js
@@ -1,205 +1,312 @@
-var socket = io('http://localhost:3000');
-$(function(){
-   // socket.emit("client-send-connect", "A Connect!!");
-
-   $(document).on('click', '.panel-heading span.icon_minim', function (e) {
-    var $this = $(this);
-    if (!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
-    } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-});
-$(document).on('focus', '.panel-footer input.chat_input', function (e) {
-    var $this = $(this);
-    if ($('#minim_chat_window').hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $('#minim_chat_window').removeClass('panel-collapsed');
-        $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-    }
-});
-$(document).on('click', '#new_chat', function (e) {
-    var size = $( ".chat-window:last-child" ).css("margin-left");
-     size_total = parseInt(size) + 400;
-    alert(size_total);
-    var clone = $( "#chat_window_1" ).clone().appendTo( ".container" );
-    clone.css("margin-left", size_total);
-});
-$(document).on('click', '.icon_close', function (e) {
-    //$(this).parent().parent().parent().parent().remove();
-    $( "#chat_window_1" ).remove();
-});
-
-//s1
-var me = {};
-me.avatar = "images/hotboy.jpg";
-
-var you = {};
-you.avatar = "images/hotgirl.jpg";
-
-function formatAMPM(date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
-}            
-
-//-- No use time. It is a javaScript effect.
-function insertChat(who, text, time = 0){
-    var control = "";
-    var date = formatAMPM(new Date());
-    
-    if (who == "me"){                     
-        control = '<div class="row msg_container base_sent">'+
-                     '<div class="col-md-10 col-xs-10">' +
-                        '<div class="messages msg_sent">' +
-                            '<p>'+ text + '</p>' +
-                            '<time>' + date + '</time>' +
-                        '</div>'+
-                     '</div>'+
-                     '<div class="col-md-2 col-xs-2 avatar">' +
-                        '<img src="'+ me.avatar +'" class=" img-responsive ">'+
-                        '</div>'+
-                    '</div>'
-    }else{
-        control = '<div class="row msg_container base_receive">'+
-                        '<div class="col-md-2 col-xs-2 avatar">' +
-                            '<img src="'+ you.avatar +'" class=" img-responsive ">'+
-                        '</div>'+                
-                        '<div class="col-md-10 col-xs-10">' +
-                        '<div class="messages msg_receive">' +
-                            '<p>'+ text + '</p>' +
-                            '<time>' + date + '</time>' +
-                        '</div>'+
-                        '</div>'+
-                        
-                    '</div>'
-    }
-    setTimeout(
-        function(){                        
-            $(".panel-body.msg_container_base").append(control);
-
-        }, time);
-    
-}
-
-function resetChat(){
-    $(".panel-body.msg_container_base").empty();
-}
-
-$("#txtMessage").on("keyup", function(e){
-    if (e.which == 13){
-        var text = $(this).val();
-        if (text !== ""){
-            insertChat("me", text);              
-            $(this).val('');
+var socket = io('https://diepchatdemo.herokuapp.com');
+$(function () {
+    //End script 
+    $(document).on('click', '.panel-heading span.icon_minim', function (e) {
+        var $this = $(this);
+        if (!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.removeClass('glyphicon-minus').addClass('glyphicon-plus');
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.removeClass('glyphicon-plus').addClass('glyphicon-minus');
         }
-    }
-});
-
-//-- Clear Chat
-resetChat();
-
-//-- Print Messages
-insertChat("me", "Hello Tom...", 0);  
-insertChat("you", "Hi, Pablo", 1500);
-insertChat("me", "What would you like to talk about today?", 3500);
-insertChat("you", "Tell me a joke",7000);
-insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
-insertChat("you", "LOL", 12000);
-//e1
-
-//s2 //show hide div
-//e2
-
-//s3 
-$("#divSayHi").hide();
-
-$("#btnRegister").click(function(){
-    let username = $("#txtUserName").val().trim();
-    if(username != ''){
-         socket.emit("client-send-userName", $("#txtUserName").val());
-         $("#lblMessage").removeClass("alert alert-danger");
-         $("#lblMessage").text("");
-         console.log("client-send-userName")
-    }
-    else{
-       $("#lblMessage").addClass("alert alert-danger");
-       $("#lblMessage").text("Please enter user name...");
-    }
-});
-
-//listen send-fail-register
-socket.on("server-send-fail-register", data =>{
-    $("#lblMessage").addClass("alert alert-danger");
-    $("#lblMessage").text(data);
-})
-socket.on("server-send-success-register", data =>{
-    $("#lblMessage").removeClass("alert alert-danger");
-    $("#lblMessage").text("");
-    $("#currentUser").text(data);
-    $("#divSayHi").show(1000);
-    $("#divLogin").hide(1000);       
-})
-socket.on("server-send-manageUsers", data =>{
-    $('ul.nav.nav-sidebar').html("");
-    data.forEach(function(element) {
-         $('ul.nav.nav-sidebar').append("<li class='userOnline'><a>" +element+ "</a></li>");
     });
+    $(document).on('focus', '.panel-footer input.chat_input', function (e) {
+        var $this = $(this);
+        if ($('#minim_chat_window').hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $('#minim_chat_window').removeClass('panel-collapsed');
+            $('#minim_chat_window').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+        }
+    });
+    $(document).on('click', '#new_chat', function (e) {
+        var size = $(".chat-window:last-child").css("margin-left");
+        size_total = parseInt(size) + 400;
+        alert(size_total);
+        var clone = $("#chat_window_1").clone().appendTo(".container");
+        clone.css("margin-left", size_total);
+    });
+    $(document).on('click', '.icon_close', function (e) {
+        //$(this).parent().parent().parent().parent().remove();
+        $("#chat_window_1").remove();
+    });
+    
+    // socket.emit("client-send-connect", "A Connect!!");
+    //s1
+    var me = {};
+    me.avatar = "images/hotboy.jpg";
+
+    var you = {};
+    you.avatar = "images/hotgirl.jpg";
+
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+
+    //-- No use time. It is a javaScript effect.
+    function insertChat(who, text, time = 0) {
+        var control = "";
+        var date = formatAMPM(new Date());
+
+        if (who == "me") {
+            control = '<div class="row msg_container base_sent">' +
+                '<div class="col-md-10 col-xs-10">' +
+                '<div class="messages msg_sent">' +
+                '<p>' + text + '</p>' +
+                '<time>' + date + '</time>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-2 col-xs-2 avatar">' +
+                '<img src="' + me.avatar + '" class=" img-responsive ">' +
+                '</div>' +
+                '</div>'
+        } else {
+            control = '<div class="row msg_container base_receive">' +
+                '<div class="col-md-2 col-xs-2 avatar">' +
+                '<img src="' + you.avatar + '" class=" img-responsive ">' +
+                '</div>' +
+                '<div class="col-md-10 col-xs-10">' +
+                '<div class="messages msg_receive">' +
+                '<p>' + text + '</p>' +
+                '<time>' + date + '</time>' +
+                '</div>' +
+                '</div>' +
+
+                '</div>'
+        }
+        setTimeout(
+            function () {
+                $(".panel-body.msg_container_base").append(control);
+
+            }, time);
+
+    }
+
+    function resetChat() {
+        $(".panel-body.msg_container_base").empty();
+    }
+
+    $("#txtMessage").on("keyup", function (e) {
+        if (e.which == 13) {
+            var msg = $(this).val();
+            let userChat = $("#chatWithUserId").text();
+            let userName = $("#currentUser").text();
+            if (msg !== "" && userChat !== "") {
+                insertChat("me", msg);
+                $(this).val('');
+                //send server chat            
+                socket.emit("client-send-message-chat", { msg: msg, userName: userName, userChat: userChat });
+            }
+        }
+    });
+
+    //-- Clear Chat
+    resetChat();
+
+    //-- Print Messages
+    insertChat("me", "Hello Tom...", 0);
+    insertChat("you", "Hi, Pablo", 1500);
+    insertChat("me", "What would you like to talk about today?", 3500);
+    insertChat("you", "Tell me a joke", 7000);
+    insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 9500);
+    insertChat("you", "LOL", 12000);
+    //e1
+
+    //s2 //show hide div
+    //e2
+
+    //s3 
+    $("#divSayHi").hide();
+
+    $("#btnRegister").click(function () {
+        let username = $("#txtUserName").val().trim();
+        if (username != '') {
+            socket.emit("client-send-userName", username);
+            $("#lblMessage").removeClass("alert alert-danger");
+            $("#lblMessage").text("");
+            console.log("client-send-userName")
+        }
+        else {
+            $("#lblMessage").addClass("alert alert-danger");
+            $("#lblMessage").text("Please enter user name...");
+        }
+    });
+
+    //listen send-fail-register
+    socket.on("server-send-fail-register", data => {
+        $("#lblMessage").addClass("alert alert-danger");
+        $("#lblMessage").text(data);
+    })
+    socket.on("server-send-success-register", data => {
+        $("#lblMessage").removeClass("alert alert-danger");
+        $("#lblMessage").text("");
+        $("#currentUser").text(data);
+        $("#divSayHi").show(1000);
+        $("#divLogin").hide(1000);
+    })
+    socket.on("server-send-manageUsers", data => {
+       //debugger
+        $('ul.nav.nav-sidebar').html("");
+        data.forEach(function (element) {
+            //$('ul.nav.nav-sidebar').append('<li class="userOnline" click="chatToUser('+ element+')"><a>' +element+ '</a></li>');
+            $('ul.nav.nav-sidebar').append('<li class="userOnline" value="' + element.data + '" code="' + element.id + '"><a>' + element.data + '</a></li>');
+        });
+    })
+    socket.on("server-send-message-newmember", data => {
+        $("#lblTypping").html(`${data} is joining, chat now !! `);
+        setTimeout(() => {
+            $("#lblTypping").html('');
+        }, 3000);
+    });
+
+    //user logout
+    //user click logout
+    $("#btnLogout").click(function () {
+        //debugger;
+        socket.emit("client-send-logout");
+        $("#divSayHi").hide(1000);
+        $("#divLogin").show(1000);
+    });
+
+    ////client type text
+    // $("#btnSendMessage").click(function(){
+    //     socket.emit("client-send-chat", $("#txtMessage").val());
+    // });
+
+    $("#txtMessage").focusin(function () {
+        socket.emit("client-stypping");
+    });
+
+    $("#txtMessage").focusout(function () {
+        socket.emit("client-stypping-out");
+    });
+
+    socket.on("server-message-typping", data => {
+        if (data) {
+            $("#lblTypping").html("<img src='images/typing05.gif' style='width:20px; height='18px' > " + data + " is typping..");
+        }
+    });
+
+    socket.on("server-message-typping-out", function () {
+        $("#lblTypping").html("");
+    });
+
+    //e3
+
+    //s4
+    //add group
+    $('[data-toggle="modal"]').tooltip();
+
+    $("#btnAddGroup").click(() => {
+        let roomName = $("#txtGroupName").val().trim();
+        if (roomName) {
+            socket.emit("client-create-room", roomName);
+        }
+        else {
+            $("#spCurrentRoom").html("Please enter name for group.");
+            setTimeout(function () {
+                $("#spCurrentRoom").html("");
+            }, 3000);
+        }
+
+    });
+
+    socket.on("server-send-inroom", function (data) {
+        $("#spCurrentRoom").html(data + " is created.");
+        setTimeout(function () {
+            $("#spCurrentRoom").html("");
+        }, 3000);
+    });
+    //e4
+
+    //s5
+    //select user or room to chat
+    $("ul.nav.nav-sidebar").on("click", ".userOnline", function (e) {
+        e.preventDefault();
+        let userSelect = $(this).attr("value");
+        let currentUser = $("#currentUser").text();
+        if (currentUser && (userSelect != currentUser)) {
+            $("#chatWithUser").html($(this).attr("value"));
+            $("#chatWithUserId").html($(this).attr("code"));
+        }
+        $("ul.nav.nav-sidebar").find(".li-active").removeClass("li-active");
+        $(this).addClass("li-active");
+        //~
+        socket.emit("client-create-room", $(this).attr("code"));
+    });
+    //e5
+
+    //s6
+    //check form chat
+    function insertChat2(who, text, time = 0) {
+        var control = "";
+        var date = formatAMPM(new Date());
+
+        if (who == "me") {
+            control = '<div class="row msg_container base_sent">' +
+                '<div class="col-md-10 col-xs-10">' +
+                '<div class="messages msg_sent">' +
+                '<p>' + text + '</p>' +
+                '<time>' + date + '</time>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-2 col-xs-2 avatar">' +
+                '<img src="' + me.avatar + '" class=" img-responsive ">' +
+                '</div>' +
+                '</div>'
+        } else {
+            control = '<div class="row msg_container base_receive">' +
+                '<div class="col-md-2 col-xs-2 avatar">' +
+                '<img src="' + you.avatar + '" class=" img-responsive ">' +
+                '</div>' +
+                '<div class="col-md-10 col-xs-10">' +
+                '<div class="messages msg_receive">' +
+                '<p>' + text + '</p>' +
+                '<time>' + date + '</time>' +
+                '</div>' +
+                '</div>' +
+
+                '</div>'
+        }
+        setTimeout(
+            function () {
+                $(".panel-body.msg_container_base").append(control);
+
+            }, time);
+
+    }
+    //e6
+
+    //s7
+    //server send message chat
+    socket.on("server-send-message-chat", data => {
+        //debugger
+        console.log(data)
+        let txtUserChat = $("#chatWithUserId").text();
+        let txtUserName = $("#currentUser").text();
+        if (data.userName !== txtUserName && txtUserChat == data.socketID) {
+            insertChat2(data.userName, data.msg);
+        }
+        else if(data.userName !== txtUserName && txtUserChat == data.userChat){
+            insertChat2(data.userName, data.msg);
+        }
+    });
+
+    // socket.on('server-send-chat2', data =>{
+    //     alert('fgdsgfdsgdfsgdfsgdfsgdf')
+    // });
+    //e7
+
+    
+
 })
-socket.on("server-send-message-newmember", data =>{
-    $("#lblTypping").html(`${data} is joining, chat now !! `);
-    setTimeout(()=>{
-        $("#lblTypping").html('');
-    }, 3000);
-});
 
-//user logout
-//user click logout
-$("#btnLogout").click(function(){
-    debugger;
-    socket.emit("client-send-logout");
-    $("#divSayHi").hide(1000);
-    $("#divLogin").show(1000);
-});
-
-////client type text
-// $("#btnSendMessage").click(function(){
-//     socket.emit("client-send-chat", $("#txtMessage").val());
-// });
-
-$("#txtMessage").focusin(function() {
-    socket.emit("client-stypping");
-});
-
-$("#txtMessage").focusout(function() {
-    socket.emit("client-stypping-out");
-});
-
-socket.on("server-message-typping", data =>{
-    if(data){
-        $("#lblTypping").html("<img src='images/typing05.gif' style='width:20px; height='18px' > "+ data +" is typping..");
-    }    
-});
-
-socket.on("server-message-typping-out", function(){
-    $("#lblTypping").html("");
-});
-
-//e3
-
-//s4
-//add group
-$('[data-toggle="addGroupToolTip"]').tooltip();
-$("#addGroup").click()
-//e4
-   
-})
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 //declare socket.io
 const io = require('socket.io')(server);
@@ -14,24 +14,36 @@ var log = console.log;
 var manageUsers = [];
 //socket connect
 io.on('connection', socket =>{
-    log(`Hello, ${socket.id} - connected.`);
+    //log(`Hello, ${socket.id} - connected.`);
     //disconnect
     socket.on('disconnect', _ =>{
-        log(`Bye, ${socket.id}`);
+        //log(`Bye, ${socket.id}`);
     });
 
     //register
     socket.on('client-send-userName', data =>{
         //log(data)
-        if(manageUsers.indexOf(data) >=0){
+        debugger
+        log(manageUsers);
+        let checkExist = true;
+        manageUsers.forEach(e =>{
+            if(e.data == data){
+                checkExist = false;
+                return;
+            }
+        })
+        if(checkExist == false){
             socket.emit('server-send-fail-register', "User name is exsist, pls create other.")
         }
-        else{
-            manageUsers.push(data);
+        else{   
+            manageUsers.push({data: data, id: socket.id});
             socket.userName = data;
+
+            log(manageUsers);
             socket.emit('server-send-success-register', data);
             io.sockets.emit('server-send-manageUsers', manageUsers);            
             socket.broadcast.emit('server-send-message-newmember', data);            
+            
         }
     });
 
@@ -50,6 +62,49 @@ io.on('connection', socket =>{
 
     socket.on("client-stypping-out",function(){
         socket.broadcast.emit("server-message-typping-out");
-    })
+    });
+
+    socket.on("client-create-room", data =>{
+        //socket.userName = data;
+        socket.join(data); //join room
+
+        let checkExist = true;
+        manageUsers.forEach(e =>{
+            if(e.data == data){
+                checkExist = false;
+                return;
+            }
+        })
+        if(checkExist){            
+            socket.roomNames = data;
+            manageUsers.push({data, id: data});
+            //update for list user in client
+            io.sockets.emit('server-send-manageUsers', manageUsers);
+            socket.emit("server-send-inroom", data);
+            socket.broadcast.emit('server-send-message-newmember', data);
+            //log(socket.adapter.rooms) //know how many rooms
+            //log(socket);
+        }        
+    });
+
+    //message send to users in room
+    //io.sockets.in(socket.roomNames).emit("server-send-chat-inroom", data);
+    
+    //leave room
+    //socket.leave(roomNames);
+
+    socket.on("client-send-message-chat", data =>{
+        log(data)
+        //socket.to(data.userChat).emit('server-send-chat2', data);  
+        data.socketID = socket.id;
+        io.sockets.in(data.userChat).emit('server-send-message-chat', data);        
+    });
 
 });
+
+function users(username, email, phone, rooms){
+    this.username = username;
+    this.email = email;
+    this.phone = phone;
+    this.rooms = rooms;
+}
